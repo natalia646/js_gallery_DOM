c67d9c6e65c60b6c8f4447e1d6dfbca9cebd38ed
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-12-02 18:59:50 +0200

    make build

---
+++ index.d6d020a4.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector(".gallery__large-img");e.addEventListener("click",function(e){if(e.preventDefault(),e){var r=e.target.closest(".list-item__link");t.src=r.href}});
+//# sourceMappingURL=index.d6d020a4.js.map
